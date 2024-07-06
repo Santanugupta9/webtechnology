384257abc8485fe2242d9cceda01a6ab91637efc
Prince Gupta <princegupta3637@gmail.com>
2024-07-06 16:23:48 +0545

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,56 @@
+// const unknown = function(...num1){
+//     console.log(num1);
+//     console.log(typeof num1);
+// }
+// unknown(1,2,3,4,5);
+//wap to check a given number is odd or even
+// const user={
+//     name:"hptech",
+//     gmail:"hptech@gmail.com"
+// }
+// showUser(user);
+// function showUser(a){
+//     console.log("user name is$(a.name) and gmail is $(a.gmail)")
+// }
+// const name = (name) =>{
+//     console.log("my name is $(name)");
+// }
+// name("prince");
+//const sum= (num1,num2)=>num1+num2;
+// const subtract= (num1,num2)=>{
+
+//      return num1-num2 }
+// console.log( subtract(1,2));
+
+// const name=(name1,name2)=>{
+//     return name1+name2;
+// }
+// b=name("shyam","hari");
+// console.log(b);
+// var c = 0;
+// function access(){
+//     let a=10;
+//     const b=20;
+//      c = 30;
+// }
+//console.log(a)
+//console.log(b)
+//console.log(c);
+// if(true){
+//     let a = 50;
+//     console.log(a);
+// }
+//console.log(a);
+const arr1=[1,2,3,4,5]
+//console.log(arr1[3]);
+//const arr2=["apple","banana","mango"]
+//console.log(arr2[2]);
+//arr1.shift(0);
+
+//console.log(arr1)
+//console.log(arr1.join(" "))
+// let a="surya";
+// console.log(a.split(""))
+// const arr=[1,2]
+console.log(arr1.splice(1,2));
+
